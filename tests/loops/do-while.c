d4303e404103a44f26ee02a99b400ad708a5b73e
Ricardo Süffert <ricardobsuffert@gmail.com>
2025-06-14 18:55:54 -0300

    fix: Allow `do... while` loops with multiple commands (#11)
    * fix syntax err with multiple cmds in do while
    
    * update unit tests
    
    * make sure var is initialized in test case
    
    * clarify code for lcmds inside if, else, and loops

--- tests/loops/do-while.c
+++ tests/loops/do-while.c
@@ -5,15 +5,20 @@ int b;
 
 int main() {
     a = 0;
+    b = 0;
     do {
         a++;
+        b += 2;
     } while (a < 2);
     printf("a: %d\n", a);
+    printf("b: %d\n", b);
 
     b = 0;
     do {
+        a += 2;
         b++;
     } while (b < 0);
+    printf("a: %d\n", a);
     printf("b: %d\n", b);
 
     return 0;
