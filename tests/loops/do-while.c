00c9bd86a777de1603e53a59141e3aa1195d7af6
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2025-06-14 18:00:47 -0300

    update unit tests

--- tests/loops/do-while.c
+++ tests/loops/do-while.c
@@ -7,13 +7,17 @@ int main() {
     a = 0;
     do {
         a++;
+        b += 2;
     } while (a < 2);
     printf("a: %d\n", a);
+    printf("b: %d\n", b);
 
     b = 0;
     do {
+        a += 2;
         b++;
     } while (b < 0);
+    printf("a: %d\n", a);
     printf("b: %d\n", b);
 
     return 0;
