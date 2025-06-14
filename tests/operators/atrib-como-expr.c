3b3957f457aec7b0033ef833bdfeb26ec75bb77f
Gustavo Lisboa Molina <80642295+GustavoLM2021@users.noreply.github.com>
2025-06-14 18:58:58 -0300

    Update atrib-como-expr.c

--- tests/operators/atrib-como-expr.c
+++ tests/operators/atrib-como-expr.c
@@ -9,9 +9,9 @@ int main() {
     b = a + 2;
     a = (b = (c = 7) * 3) + 2;
 
-    printf("a (23) = %d\n", a);
-    printf(" b (21)=  %d\n", b);
-    printf(" c (7)=  %d\n", c);
+    printf("a (23)=%d\n", a);
+    printf("b (21)=%d\n", b);
+    printf("c (7)=%d\n", c);
 
     return 0;
 }
