88377f6fb57322cc38601a864f4fbdc06cb7ab83
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2025-06-15 22:16:28 -0300

    add fibonacci calculator

---
+++ tests/misc/fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int n;
+int i;
+
+int a;
+int b;
+int next;
+
+int main() {
+    n = 10;
+
+    a = 0;
+    b = 1;
+
+    printf("Fibonacci Series:\n");
+
+    for(int i=0; i<=n; i++) {
+        printf(" %d\n", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
