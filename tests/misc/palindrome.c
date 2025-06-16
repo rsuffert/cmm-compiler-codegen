87844105ebc2abd1fe2027825ad4f892ccb4af77
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2025-06-15 22:38:28 -0300

    add number palindrome checker

---
+++ tests/misc/palindrome.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int n;
+int original;
+int reversed;
+int digit;
+
+int main() {
+    n = 123321;
+    original = n;
+    reversed = 0;
+
+    while (n > 0) {
+        digit = digit % 10;
+        reversed = reversed * 10 + digit;
+        n = n / 10;
+    }
+
+    if (reversed == original) {
+        printf("Is palindrome!\n");
+    } else {
+        printf("Is not palindrome...\n");
+    }
+
+    return 0;
+}
